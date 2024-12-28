c642c5799885a6def75fbd5f03fc9a72adf27e3b
yusufalp <alpyusuf007@gmail.com>
2024-12-28 11:49:27 -0800

    add image to modal

--- src/app/assessment/modal/page.js
+++ src/app/assessment/modal/page.js
@@ -1,10 +1,20 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function ModalPage() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <div className={styles.logo}>
+          <Image
+            src="/info.svg"
+            alt="info icon"
+            width={48}
+            height={48}
+            priority
+          />
+        </div>
         <section className={styles.intro}>
           <h2 className={styles.introTitle}>Thank You!</h2>
           <p className={styles.introDescription}>
